8a050d86387770d869bbbdc59ad0aa97970cb83c
Fai-Web-Lab <fai.alsharekh@gmail.com>
2026-03-14 05:40:01 +0300

    Add SQL scripts for database schema and initial data

---
+++ part3/SQL/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO User (id, first_name, last_name, email, password, is_admin)
+VALUES ('36c9050e-ddd3-4c3b-9731-9f487208bbc1', 'Admin', 'HBnB', 'admin@hbnb.io', '$2b$12$6G3x9uR7k9p3/8Hj9qW1ueXFjR2eK/VzY8tW5S6N0mI2oP7Q6L8S.', TRUE);
+
+INSERT INTO Amenity (id, name) VALUES ('56781234-abcd-4321-bcde-1234567890ab', 'WiFi');
+INSERT INTO Amenity (id, name) VALUES ('98765432-fedc-1234-abcd-0987654321ba', 'Swimming Pool');
+INSERT INTO Amenity (id, name) VALUES ('12345678-aaaa-bbbb-cccc-8765432109de', 'Air Conditioning');
